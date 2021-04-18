c247a5ff80c0816c2f010b503e882ecf4647b72b
Vipin Goel <vipin@snapcap.com>
2021-04-18 00:24:32 -0400

    initial commmit

---
+++ pkg/apis/compositioncontroller/v1alpha1/types.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Composition is a specification for a Composition resource
+type Composition struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CompositionSpec   `json:"spec"`
+	Status CompositionStatus `json:"status"`
+}
+
+// CompositionSpec is the spec for a Composition resource
+type CompositionSpec struct {
+	Replicas *int32 `json:"replicas"`
+}
+
+// CompositionStatus is the status for a Composition resource
+type CompositionStatus struct {
+	AvailableReplicas int32 `json:"availableReplicas"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// FooList is a list of Foo resources
+type CompositionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Composition `json:"items"`
+}
